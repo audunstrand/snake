7404ec07234eeed9b0a1d5f454ecd8bef4a2ebcc
Audun Fauchald Strand <audun.fauchald.strand@nav.no>
2021-07-20 22:59:52 +0200

    just kotlin

--- lib/src/main/kotlin/snake/Game.kt
+++ lib/src/main/kotlin/snake/Game.kt
@@ -23,17 +23,23 @@ class Game(
     fun won() = board.noMoreFood()
     fun validMove() = board.validMoves(snake).getOrElse(0) { NO_TURN }
     fun tick(turn: TURN): Boolean {
-        val head = snake.move(turn)
-        return when {
-            snake.eatsHimself() -> false.also { println("eats himself ${points()} points") }
-            board.isWall(head) -> false.also { println("hitsWall ${points()} points") }
-            board.isFood(head) -> true.also {
-                snake.eat()
-                board.removeFood(head)
+        return snake.move(turn).let { newHead ->
+            when {
+                snake.eatsHimself() -> false.also { println("eats himself ${points()} points") }
+                hitsWall(newHead) -> false.also { println("hitsWall ${points()} points") }
+                hitsFood(newHead) -> true.also { eat(newHead) }
+                else -> true
             }
-            else -> true
         }.also { board.printWith(snake) }
     }
+
+    private fun eat(newHead: Point) {
+        snake.eat()
+        board.removeFood(newHead)
+    }
+
+    private fun hitsFood(newHead: Point) = board.isFood(newHead)
+    private fun hitsWall(newHead: Point) = board.isWall(newHead)
 
     enum class TURN {
         RIGHT,
